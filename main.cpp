be3ed1fb9caaad75a231c00cb5e8f243aa3b1a3a
mellw0101 <melwivensson@gmail.com>
2024-02-25 20:48:53 +0100

    Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@ using namespace std;
 void __4070_ti()
 {
     string
-        kawpow("30 Mh"),
-        nexa  ("160 Mh");
+        kawpow ("30 Mh"),
+        nexa   ("160 Mh - bzminer"),
+        skydoge("41 Mh - wildrig");
     
+}
+
+void __6750_xt()
+{
+    string
+        kawpow ("27 Mh"),
+        nexa   ("50 Mh - bzminer"),
+        skydoge("18 Mh - wildrig");
+
 }
 
 int main()
